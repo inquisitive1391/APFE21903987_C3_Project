920ffde0c192794ef4a0158932b3dd6a5ff8b29a
inquisitive1391 <66990135+inquisitive1391@users.noreply.github.com>
2021-06-30 18:59:41 +0530

    Part 2: Solution

--- Restaurant_project/Restaurant_project/src/test/java/RestaurantTest.java
+++ Restaurant_project/Restaurant_project/src/test/java/RestaurantTest.java
@@ -8,16 +8,29 @@ import static org.junit.jupiter.api.Assertions.*;
 class RestaurantTest {
     Restaurant restaurant;
     //REFACTOR ALL THE REPEATED LINES OF CODE
+    public void restaurantCreation(){
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant = new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+    }
 
     //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     //-------FOR THE 2 TESTS BELOW, YOU MAY USE THE CONCEPT OF MOCKING, IF YOU RUN INTO ANY TROUBLE
     @Test
     public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
+        restaurantCreation();
+        restaurant.setClosingTime(LocalTime.now().plusMinutes(10));
+        assertTrue(restaurant.isRestaurantOpen());
         //WRITE UNIT TEST CASE HERE
     }
 
     @Test
     public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
+        restaurantCreation();
+        restaurant.setClosingTime(LocalTime.now().minusMinutes(10));
+        assertFalse(restaurant.isRestaurantOpen());
         //WRITE UNIT TEST CASE HERE
 
     }
